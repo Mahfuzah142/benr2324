b0e4e7f9091bd2a0eab0869ceb52bf5dee0c2fc4
Mahfuzah142 <b022210169@student.utem.edu.my>
2024-04-15 15:40:46 +0800

    postman

--- index.js
+++ index.js
@@ -1,19 +1,79 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5555;
+const port = process.env.PORT || 3000;
+const bcrypt = require('bcrypt');
 
 app.use(express.json())
+// new user registration
+app.post('/user', async (req, res) => {
+  // console.log(req.body)
 
-app.get('/', (req, res) => {
-   res.send('Hello World!')
+  const hash = bcrypt.hashSync(req.body.password, 10);
+  let result = await client.db('maybank2u').collection('users').insertOne(
+    {
+      username: req.body.username,
+      password: hash,
+      name: req.body.name,
+      email: req.body.email
+    }
+  )
+  res.send(result)
+})
+
+app.post('/login',async(req, res) => {
+  //usename: req.body.username
+  //password: req.body.password
+
+  //step 1: check if username exist
+  let result = await client.db("maybank2u").collection("users").findOne(
+  {
+    username: req.body.username
+  })
+  
+  if(!result) {
+    res.send("username not found")
+  }else{
+    //Step 2: Check if password is correct
+    //result.password = req.body.password
+
+    if(bcrypt.compareSync(req.body.password, result.password) == true){
+      res.send("Login successfully")
+    }else{
+      res.send("Wrong password")
+    }
+  }
+  console.log(result)
+
+})
+
+// get user profile
+app.get('/user/:siapadia/:emaildia', async (req, res) => {
+  // findOne
+  let result = await client.db('maybank2u').collection('users').findOne({
+    username: req.params.siapadia,
+    email: req.params.emaildia
+  })
+  res.send(result)
+})
+
+// update user account
+app.patch('/user', (req, res) => {
+  // updateOne
+  console.log('update user profile')
+})
+
+// delete user account
+app.delete('/user', (req, res) => {
+  // deleteOne
+  console.log('delete user account')
 })
 
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
+  console.log("Example app listening on port ${port}")
 })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://nurmahfuzah03:Mahfuzah03@mahfuzah03.blu6t3a.mongodb.net/?retryWrites=true&w=majority&appName=Mahfuzah03";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://soo:passwordbaharu@cluster0.nehnjjb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,20 +86,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
+    // Connect the client to the server  (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    
-    //await client.db("admin").command({ ping: 1 });
-    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    await client.db('Mahfuzah03').collection('students').insertOne({
-      name: 'Mahfuzah',
-      age: 21
-    })
+    console.log('Connected successfully to MongoDB')
   } finally {
-    // Ensures that the client will close when you finish/error
-   // await client.close();
   }
 }
 run().catch(console.dir);
-
